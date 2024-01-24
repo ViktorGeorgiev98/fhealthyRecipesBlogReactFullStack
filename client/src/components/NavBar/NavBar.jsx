b2f962c68046bc1609f2a64d25ad876e16a9c0dc
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-01-24 18:14:42 +0200

    added all Link elements for nav bar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -7,13 +7,30 @@ const NavBar = () => {
                 <img className="logo-image" src="https://png.pngtree.com/png-clipart/20230804/original/pngtree-healthy-recipes-prescription-palatable-sound-vector-picture-image_9509396.png" alt="logo"/>
             </Link>
             <ul className='nav-bar-elements'>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>
+                    <Link to='/home'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/login'>Login</Link>
+                </li>
+                <li>
+                    <Link to='/register'>Register</Link>
+                </li>
+                <li>
+                    <Link to='/createRecipe'>Add Recipe</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/recipes'>Recipes</Link>
+                </li>
+                <li>
+                    <Link to='/user/profile'>My profile</Link>
+                </li>
+                <li>
+                    <Link to='/logout'>Logout</Link>
+                </li>
             </ul>
         </nav>
     )
