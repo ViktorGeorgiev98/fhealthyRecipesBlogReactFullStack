bc837e5264453fed455c667847623c7ff1fdafb2
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-01-27 18:46:31 +0200

    created controlled reigister form and css for it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import HomePage from './components/Home/HomePage';
 import Login from './components/LoginRegister/Login';
+import Register from './components/LoginRegister/Register';
 import NavBar from './components/NavBar/NavBar'
 import { Routes, Route } from 'react-router-dom';
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </>
   )
--- client/src/components/LoginRegister/Login.jsx
+++ client/src/components/LoginRegister/Login.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
 const Login = () => {
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     return (
         <div className="login-page">
             <form className="login-form">
                 <h1>Login</h1>
-                <input type="text" placeholder="email" onChange={(e) => setUsername(e.currentTarget.value)}/>
+                <input type="text" placeholder="email" onChange={(e) => setEmail(e.currentTarget.value)}/>
                 <input type="text" placeholder="password" onChange={(e) => setPassword(e.currentTarget.value)}/>
                 <button type="submit">Login</button>
             </form>
