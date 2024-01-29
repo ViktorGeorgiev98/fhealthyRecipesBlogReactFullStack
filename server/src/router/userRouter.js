edbc1833d0f09897b249e3649a3118798881cc29
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-01-29 12:04:23 +0200

    created error handler utils method for server related requests

--- server/src/router/userRouter.js
+++ server/src/router/userRouter.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const { extractErrorMessage } = require('../utils/errorHandler');
 
-
-router.post('/register', (req, res) => {
-    
+router.post('/register', async (req, res) => {
+    const {username, email, password} = req.body;
+    co
 })
 
 
