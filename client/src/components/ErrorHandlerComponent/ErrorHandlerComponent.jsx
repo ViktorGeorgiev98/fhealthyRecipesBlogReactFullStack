59bb511307b1d7b9d85333d6bf5b183a3e1ea240
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-02-04 13:39:15 +0200

    css for error pop up window

--- client/src/components/ErrorHandlerComponent/ErrorHandlerComponent.jsx
+++ client/src/components/ErrorHandlerComponent/ErrorHandlerComponent.jsx
@@ -6,9 +6,11 @@ const ErrorHandlerComponent = (props) => {
     
     return (
         <div className="error-window-pup-up">
-            <h3>You have encountered an error...</h3>
-            <p>{props.errorMessage}</p>
-            <button className="btn-close-error-window" onClick={() => props.setError(false)}>OK</button>
+            <div className='error-window'>
+                <h3>You have encountered an error...</h3>
+                <p>{props.errorMessage}</p>
+                <button className="btn-close-error-window" onClick={() => props.setError(false)}>OK</button>
+            </div>
         </div>
     )
 }
